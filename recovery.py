50305292a9d67e5983b4780ab93bf140fd4fc831
Tungishbay <43431475+Tungishbay@users.noreply.github.com>
2018-11-29 15:28:30 +0600

    Update recovery.py

--- recovery.py
+++ recovery.py
@@ -74,7 +74,7 @@ if "HEROKU" in list(os.environ.keys()):
     @server.route("/")
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url="")
+        bot.set_webhook(url="https://stark-mountain-74246.herokuapp.com/bot")
         return "?", 200
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
 else:
