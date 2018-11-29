71615eb0679afe89c34951c67940a74f953800b7
tungishbay <tako_98_01@mail.ru>
2018-11-29 15:02:31 +0600

    'Start'

--- recovery.py
+++ recovery.py
@@ -1,43 +1,82 @@
-import requests
-from time import sleep
+import telebot, os, logging, psycopg2
+from flask import Flask, request
 
-url = 'https://api.telegram.org/bot773839668:AAEHel4AUB-ZSIqpHI_HqOXyPbgErpebk_g/'
+TOKEN = "773839668:AAEHel4AUB-ZSIqpHI_HqOXyPbgErpebk_g"
+bot = telebot.TeleBot(TOKEN)
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
 
-def get_updates_json(request):
-	params = {'timeout': 30, 'offset': None}
-	response = requests.get(request + 'getUpdates')
-	return response.json()
-
-def last_update(data):
-	results = data['result']
-	total_updates = len(results) - 1
-	return results[total_updates]
-
-def send_info(update):
-	chat_id = update['message']['chat']['id']
-	first_name = update['message']['from']['first_name']
-	username = update['message']['from']['username']
-	user_id = update['message']['from']['id']
-	is_bot = update['message']['from']['is_bot']
-	language = update['message']['from']['language_code']
-
-	if is_bot:
-		bot = '\nYou are bot'
-
-	else:
-		bot = '\nYou are not bot'
-	text = 'Your data:\nName: ' + first_name + '\nUsername: ' + username + '\nID: ' + str(user_id) + bot + '\nLanguage: ' + language
-	params = {'chat_id': chat_id, 'text': text}
-	response = requests.post(url + 'sendMessage', data = params)
-	return response
-
-def main():
-	update_id = last_update(get_updates_json(url))['update_id']
+@bot.message_handler(commands=['start'])
+def start(message):
+	cur.execute("SELECT * FROM Users")
 	while True:
-		if update_id == last_update(get_updates_json(url))['update_id']:
-			send_info(last_update(get_updates_json(url)))
-			update_id += 1
-		sleep(1)
+		row = cur.fetchone()
+		if row == None:
+			bot.send_message(message.chat.id, 'Hello, ' + message.from_user.first_name)
+			break
+		else:
+			if row[0] == message.from_user.id:
+				mess = "ID: " + str(row[0])
+				if row[1] != "-":
+					mess += "\nUsername: " + row[1]
+				mess += "\nFirst name: " + row[2]
+				if row[3] != "-":
+					mess += "\nLast name: " + row[3]
+				mess += "\nFirst message: \"" + row[4] +"\""
+				bot.send_message(message.chat.id, mess)
+				break
 
-if __name__ == '__main__':
-	main()
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+	cur.execute("SELECT * FROM Users")
+	while True:
+		row = cur.fetchone()
+		if row == None:
+			mess = 'INSERT INTO Users VALUES(' + str(message.from_user.id)
+			if message.from_user.username != None:
+				mess += ', \'' + message.from_user.username + '\', \''
+			else:
+				mess += ', \'-\', \''
+			mess += message.from_user.first_name + '\', \''
+			if message.from_user.last_name != None:
+				mess += message.from_user.last_name + '\', \''
+			else:
+				mess += '-\', \''
+			mess += message.text + '\')'
+			cur.execute(mess)
+			conn.commit()
+			bot.send_message(message.chat.id, "Ваши данные в базе данных")
+			break
+		else:
+			if row[0] == message.from_user.id:
+				mess = "ID: " + str(row[0])
+				if row[1] != "-":
+					mess += "\nUsername: " + row[1]
+				mess += "\nFirst name: " + row[2]
+				if row[3] != "-":
+					mess += "\nLast name: " + row[3]
+				mess += "\nFirst message: \"" + row[4] +"\""
+				bot.send_message(message.chat.id, mess)
+				break
+
+
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+		
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="")
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
